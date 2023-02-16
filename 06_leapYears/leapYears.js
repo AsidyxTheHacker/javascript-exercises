30c8bd16e128817d561fb045f1bfdc76290e97c4
Adam <adammiller.2000nj@gmail.com>
2023-02-15 23:11:02 -0500

    finished 6th task

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divBy4 = year % 4;
+    const divBy100 = year % 100;
+    const divBy400 = year % 400;
+    if (divBy4 === 0) {
+        if (divBy100 === 0) {
+            if (divBy400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 
-};
-
+    // return year % 4 === 0 && ( year % 100 !== 0 || year % 400 == 0 )
+}
 // Do not edit below this line
 module.exports = leapYears;
