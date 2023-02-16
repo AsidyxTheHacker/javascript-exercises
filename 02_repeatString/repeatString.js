6099ff3ba11e322b2d858903441f9f574c23f22b
Adam <adammiller.2000nj@gmail.com>
2023-02-15 22:31:13 -0500

    finished 2nd task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = "";
+    for(i = 0; i < num; i++) {
+        newString = newString.concat(string);
+    }
+        if (num < 0) {
+            return 'ERROR';
+        }
+    return newString;
 };
 
 // Do not edit below this line
